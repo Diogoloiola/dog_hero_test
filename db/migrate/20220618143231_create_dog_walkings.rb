aef28cbbbadd652159b14f862a56602c252f2504
Diogo Loiola <diogomouraloiola@gmail.com>
2022-06-18 11:34:42 -0300

    create model dog_walking

---
+++ db/migrate/20220618143231_create_dog_walkings.rb
@@ -0,0 +1,16 @@
+class CreateDogWalkings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dog_walkings do |t|
+      t.string :status
+      t.date :appointment_date
+      t.float :price
+      t.integer :durantion
+      t.decimal :latitude, precision: 10, scale: 6
+      t.decimal :longitude, precision: 10, scale: 6
+      t.date :start_tour
+      t.date :end_tour
+
+      t.timestamps
+    end
+  end
+end
